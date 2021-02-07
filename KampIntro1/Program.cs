112f69e048175c76aeb6c3c6bb82d2e61158f01e
Mert Yılmaz <b202102029@subu.edu.tr>
2021-02-07 07:48:11 +0300

    Add project files.

---
+++ KampIntro1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KampIntro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //değer tutucu, alias
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+        }
+    }
+}
